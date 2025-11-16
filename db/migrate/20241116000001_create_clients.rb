d008bec94a7516638a46ecc07a6059451494679a
Thejo Kondubhatla <thejo.kondubhatla@gmail.com>
2025-11-16 14:25:54 -0600

    Add Client model and corresponding migration for it
    This adds Client model and the corresponding migration for the same that adds
    the name field(with index on it)

---
+++ db/migrate/20241116000001_create_clients.rb
@@ -0,0 +1,11 @@
+class CreateClients < ActiveRecord::Migration[7.2]
+  def change
+    create_table :clients do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+
+    add_index :clients, :name
+  end
+end
